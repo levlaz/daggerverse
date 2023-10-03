faa4566289d26e254c3a87a306aca2dd87be5d4c
Lev Lazinskiy <lev@dagger.io>
2023-10-03 14:45:34 +0100

    easter egg

--- get-ip/main.go
+++ get-ip/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 type GetIp struct {
 	Ctr *Container
@@ -21,5 +24,36 @@ func (m *GetIp) Run() *Container {
 	return m.Ctr.
 		WithEnvVariable("CACHEBUSTER", time.Now().String()).
 		WithExec([]string{"env"}).
+		WithExec([]string{"dig", "+short", "myip.opendns.com", "@resolver1.opendns.com"})
+}
+
+// NSFW: This dumps the entire host env into the container
+func (m *GetIp) RunEnv(env string) *Container {
+	var affirmation string
+
+	m.initBaseContainer()
+
+	is_ci := strings.Contains(env, "CIRCLECI")
+	if is_ci == true {
+		affirmation = `
+		You deserve better than this 1000 line YAML file.
+		You deserve better than waiting 12 minutes for a git clone.
+		You deserve better. 
+
+		Don't worry, help is on the way. 
+
+		
+		██████╗░░█████╗░░██████╗░░██████╗░███████╗██████╗░░░░██╗░█████╗░
+		██╔══██╗██╔══██╗██╔════╝░██╔════╝░██╔════╝██╔══██╗░░░██║██╔══██╗
+		██║░░██║███████║██║░░██╗░██║░░██╗░█████╗░░██████╔╝░░░██║██║░░██║
+		██║░░██║██╔══██║██║░░╚██╗██║░░╚██╗██╔══╝░░██╔══██╗░░░██║██║░░██║
+		██████╔╝██║░░██║╚██████╔╝╚██████╔╝███████╗██║░░██║██╗██║╚█████╔╝
+		╚═════╝░╚═╝░░╚═╝░╚═════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝╚═╝╚═╝░╚════╝░
+		`
+	}
+
+	return m.Ctr.
+		WithEnvVariable("CACHEBUSTER", time.Now().String()).
+		WithExec([]string{"echo", affirmation}).
 		WithExec([]string{"dig", "+short", "myip.opendns.com", "@resolver1.opendns.com"})
 }
