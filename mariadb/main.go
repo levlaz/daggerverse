a49946563219c1986c204e6cf657c063e8a94756
Lev Lazinskiy <lev@dagger.io>
2024-01-23 12:05:52 -0800

    Revert "change function name"
    This reverts commit b5ea3a391c0898561b00b89b9dfa84408c40c424.

--- mariadb/main.go
+++ mariadb/main.go
@@ -3,7 +3,7 @@ package main
 type Mariadb struct{}
 
 // example usage: dagger up --port 3306:3306 serve
-func (m *Mariadb) ServeDB() *Service {
+func (m *Mariadb) Serve() *Service {
 	return dag.Container().
 		From("mariadb:latest").
 		WithEnvVariable("MARIADB_ALLOW_EMPTY_ROOT_PASSWORD", "1").
