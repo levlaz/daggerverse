b5ea3a391c0898561b00b89b9dfa84408c40c424
Lev Lazinskiy <lev@dagger.io>
2024-01-23 12:03:49 -0800

    change function name

--- mariadb/main.go
+++ mariadb/main.go
@@ -3,7 +3,7 @@ package main
 type Mariadb struct{}
 
 // example usage: dagger up --port 3306:3306 serve
-func (m *Mariadb) Serve() *Service {
+func (m *Mariadb) ServeDB() *Service {
 	return dag.Container().
 		From("mariadb:latest").
 		WithEnvVariable("MARIADB_ALLOW_EMPTY_ROOT_PASSWORD", "1").
