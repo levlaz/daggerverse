dbff6f3758c4596e015578b980a523de1a13911b
Lev Lazinskiy <lev@dagger.io>
2024-04-01 23:03:56 -0700

    add new module files

---
+++ dragonfly/dagger/src/main.py
@@ -0,0 +1,36 @@
+"""Dragonfly DB Module.
+
+This module provides a simpele way to use DragonflyDB as a part of your 
+CI/CD pipeline.
+"""
+
+import dagger
+from dagger import dag, function, object_type
+
+@object_type
+class Dragonfly:
+    
+    @function
+    def serve(self) -> dagger.Service:
+        """Return DragonflyDB as service.
+        
+        example usage: dagger serve up --ports 6379:6379
+        """
+        return (
+            dag.container()
+            .from_("docker.dragonflydb.io/dragonflydb/dragonfly:latest")
+            .with_exposed_port(6379)
+            .as_service()
+        )
+
+    @function
+    def debug(self) -> dagger.Container:
+        """Return DragonflyDB container.
+
+        example usage: dagger call debug terminal
+        """
+        return (
+            dag.container()
+            .from_("docker.dragonflydb.io/dragonflydb/dragonfly:latest")
+            .with_entrypoint(["bash"])
+        )
