a46591542fa07c3f2b607a58fc99a69c64bbe2bd
Lev Lazinskiy <lev@dagger.io>
2024-04-05 16:44:13 -0400

    Publish cloud-utils modudle
    Cloud Utils is a useful set of utilities for working with Dagger Cloud.
    
    --
    Remove org-id
    
    /traces/{traceID} will now automatically redirect, so the org-id is not
    stricly required.

--- cloud-utils/dagger/src/main/__init__.py
+++ cloud-utils/dagger/src/main/__init__.py
@@ -11,17 +11,14 @@ from dagger import dag, function, object_type, Doc
 class CloudUtils:
     @function
     def trace_url(
-        self,
-        # TODO org would be better if dagger login worked, I would grab it from 
-        # that context instead of needing to pass it in like this.
-        org: Annotated[str, Doc("Dagger Cloud Org Name")]
+        self
     ) -> str:
         """Parse Environment Variables and Return Dagger Cloud Trace URL"""
         full = os.getenv("TRACEPARENT")
         # TODO this is a magic number, is it always guaranteed to be this?
         if full is not None:
             trace = full.split("-")[1]
-            url = f'https://dagger.cloud/{org}/traces/{trace}'
+            url = f'https://dagger.cloud/traces/{trace}'
             return url 
         
         return None
