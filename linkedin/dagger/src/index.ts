7f51f1eef157c8f123c372d864a71f2d7d178b38
Lev Lazinskiy <lev@dagger.io>
2024-04-23 22:03:43 -0700

    Release LinkedIn v0.1.0
    * This commit introduces the Dagger LinkedIn module that allows you to
    add thinkboi content directly from your Dagger pipeline.
    * Update CircleCI config to point to the SHA instead of TAG in thr final
      URL that we send out to Twitter and Mastodon.

---
+++ linkedin/dagger/src/index.ts
@@ -0,0 +1,55 @@
+/**
+ * LinkedIn Module
+ *
+ * This module allows you to interacte with the LinkedIn API from a Dagger 
+ * pipeline.
+ *
+ */
+
+import { dag, Container, Directory, object, func, Secret } from "@dagger.io/dagger"
+import { RestliClient } from "linkedin-api-client"
+
+@object()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class Linkedin {
+
+  /**
+   * Post message to LinkedIn
+   * 
+   * Example usage: dagger call post --token env:LINKEDIN_TOKEN --text "hello"
+   * 
+   * More information on how to get started with the LinkedIn
+   * API can be found here: https://github.com/linkedin-developers/linkedin-api-js-client?tab=readme-ov-file#pre-requisites
+   * 
+   * @param token LinkedIn 3-legged access token 
+   * @param text Body of LinkedIn message 
+   */
+  @func()
+  async post(token: Secret, text: string): Promise<string>{
+    const restliClient = new RestliClient();
+
+    const user_response = await restliClient.get({
+      resourcePath: '/userinfo',
+      accessToken: await token.plaintext()
+    })
+
+    const response = await restliClient.create({
+      resourcePath: '/posts',
+      entity: {
+        author: `urn:li:person:${user_response.data.sub}`,
+        lifecycleState: 'PUBLISHED',
+        visibility: 'PUBLIC', 
+        commentary: text,
+        distribution: {
+          feedDistribution: 'MAIN_FEED',
+          targetEntities: [],
+          thirdPartyDistributionChannels: []
+        }
+      },
+      accessToken: await token.plaintext(),
+      versionString: '202304'
+    })
+
+    return JSON.stringify(response.createdEntityId)
+  }
+}
