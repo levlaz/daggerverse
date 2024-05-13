97885d3bab7c22abb16093dc73ce041274a2968a
Lev Lazinskiy <lev@dagger.io>
2024-05-13 16:16:33 -0700

    Release v0.2.0 of Docusaurus module
    This commit splits out the build and serve portion of this module so
    that it makes it easier to debug your site if needed. We also renamed
    run to serve to make it more obvious what is going on. The end result is
    a simplified module that does not require any chaining in order to work.

--- docusaurus/dagger/src/index.ts
+++ docusaurus/dagger/src/index.ts
@@ -6,7 +6,7 @@
  * 
  * Example Usage:
  * 
- * `dagger call -m github.com/levlaz/daggerverse/docusaurus run --dir "/src/docs" --src https://github.com/kpenfound/dagger#kyle/docs-239-convert-secrets as-service up`
+ * `dagger call -m github.com/levlaz/daggerverse/docusaurus serve --dir "/src/docs" --src https://github.com/kpenfound/dagger#kyle/docs-239-convert-secrets`
  * 
  * The example above shows how to grab a remote git branch, the basic 
  * structure is https://github.com/$USER/$REPO#$BRANCH. The `src` argument can 
@@ -16,22 +16,20 @@
  * your fork and preview the docs without needing to install any local dependencies 
  * or have to remember how to fetch remote branches locally. 
  * 
- * Since our run function returns a dagger `Container` type, we can run it as a 
- * service by chaining `as-service up` to the end of our invocation.
  */
 
-import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
+import { dag, Void, Container, Directory, object, func } from "@dagger.io/dagger"
 
 @object()
 class Docusaurus {
   /**
-   * Run docs site locally
+   * Return container for running docusaurus with docs mounted.
    * 
    * @param src The source directory of your docusaurus site, this can be a local directory or a remote git repo
    * @param dir Optional working directory if you need to execute docusaurus commands outside of your root
    */
   @func()
-  run(src: Directory, dir = "/src"): Container {
+  build(src: Directory, dir = "/src"): Container {
     return dag
       .container()
       .from("node:lts-alpine")
@@ -44,6 +42,19 @@ class Docusaurus {
       .withMountedCache("/root/.npm", dag.cacheVolume("node-docusaurus-root"))
       .withExposedPort(3000)
       .withExec(["npm", "install"])
+  }
+
+  /**
+   * Run docs site locally
+   * 
+   * @param src The source directory of your docusaurus site, this can be a local directory or a remote git repo
+   * @param dir Optional working directory if you need to execute docusaurus commands outside of your root
+   */
+  @func()
+  serve(src: Directory, dir = "/src"): Promise<Void> {
+    return this.build(src, dir)
       .withExec(["npm", "start", "--", "--host", "0.0.0.0"])
+      .asService()
+      .up()
   }
 }
