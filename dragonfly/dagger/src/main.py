86fd5160f4ec9569aa193567a743447066f8cf59
Lev Lazinskiy <lev@dagger.io>
2024-05-21 10:56:05 -0700

    Release v0.1.3 of DragonflyDB Module
    * expose prots when running in debug mode

--- dragonfly/dagger/src/main.py
+++ dragonfly/dagger/src/main.py
@@ -1,19 +1,18 @@
 """Dragonfly DB Module.
 
-This module provides a simple way to use DragonflyDB as a part of your 
+This module provides a simple way to use DragonflyDB as a part of your
 CI/CD pipeline.
 """
 
 import dagger
 from dagger import dag, function, object_type
 
+
 @object_type
 class Dragonfly:
-    
     @function
     def serve(self) -> dagger.Service:
         """Return DragonflyDB as service.
-        
         example usage: dagger serve up --ports 6379:6379
         """
         return (
@@ -32,5 +31,6 @@ class Dragonfly:
         return (
             dag.container()
             .from_("docker.dragonflydb.io/dragonflydb/dragonfly:latest")
+            .with_exposed_port(6379)
             .with_entrypoint(["bash"])
         )
