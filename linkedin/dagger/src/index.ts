9bbcd60663037c8b7c20897b060dc636a8abdb04
Lev Lazinskiy <lev@levlaz.org>
2024-07-11 17:55:02 -0700

    remove version string from API
    I was getting a "426 upgrade required" error message when trying to use
    this module. The docs do not make it clear which version of the API I am
    supposed to be using. So I tried to call it without specifying the
    version at all and this seemed to work just fine.
    
    Signed-off-by: Lev Lazinskiy <lev@levlaz.org>

--- linkedin/dagger/src/index.ts
+++ linkedin/dagger/src/index.ts
@@ -1,55 +1,65 @@
 /**
  * LinkedIn Module
  *
- * This module allows you to interacte with the LinkedIn API from a Dagger 
+ * This module allows you to interacte with the LinkedIn API from a Dagger
  * pipeline.
  *
  */
 
-import { dag, Container, Directory, object, func, Secret } from "@dagger.io/dagger"
-import { RestliClient } from "linkedin-api-client"
+import {
+  dag,
+  Container,
+  Directory,
+  object,
+  func,
+  Secret,
+} from "@dagger.io/dagger";
+import { RestliClient } from "linkedin-api-client";
 
 @object()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Linkedin {
-
   /**
    * Post message to LinkedIn
-   * 
+   *
    * Example usage: dagger call post --token env:LINKEDIN_TOKEN --text "hello"
-   * 
+   *
    * More information on how to get started with the LinkedIn
    * API can be found here: https://github.com/linkedin-developers/linkedin-api-js-client?tab=readme-ov-file#pre-requisites
-   * 
-   * @param token LinkedIn 3-legged access token 
-   * @param text Body of LinkedIn message 
+   *
+   * @param token LinkedIn 3-legged access token
+   * @param text Body of LinkedIn message
    */
   @func()
-  async post(token: Secret, text: string): Promise<string>{
+  async post(token: Secret, text: string): Promise<string> {
     const restliClient = new RestliClient();
 
     const user_response = await restliClient.get({
-      resourcePath: '/userinfo',
-      accessToken: await token.plaintext()
-    })
+      resourcePath: "/userinfo",
+      accessToken: await token.plaintext(),
+    });
 
     const response = await restliClient.create({
-      resourcePath: '/posts',
+      resourcePath: "/posts",
       entity: {
         author: `urn:li:person:${user_response.data.sub}`,
-        lifecycleState: 'PUBLISHED',
-        visibility: 'PUBLIC', 
+        lifecycleState: "PUBLISHED",
+        visibility: "PUBLIC",
         commentary: text,
         distribution: {
-          feedDistribution: 'MAIN_FEED',
+          feedDistribution: "MAIN_FEED",
           targetEntities: [],
-          thirdPartyDistributionChannels: []
-        }
+          thirdPartyDistributionChannels: [],
+        },
       },
       accessToken: await token.plaintext(),
-      versionString: '202304'
-    })
+    });
 
-    return JSON.stringify(response.createdEntityId)
+    return JSON.stringify(response.createdEntityId);
+  }
+
+  @func()
+  async debug(source: Directory): Promise<Container> {
+    return dag.container().from("node:lts").withDirectory("/src", source);
   }
 }
