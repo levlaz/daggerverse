ad4fcdbf094eaa56cbf1572012f034b74cd54386
Lev Lazinskiy <lev@levlaz.org>
2024-10-17 21:52:07 -0700

    Release bluesky/v0.3.0
    Refactor to use Typescript SDK so its easier to support the RichText format.

--- bluesky/src/main/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Bluesky Dagger Module
-
-Send posts to Bluesky from your Dagger Pipelines
-"""
-from .main import Bluesky
--- bluesky/test/src/main/__init__.py
+++ bluesky/test/src/main/__init__.py
@@ -12,10 +12,26 @@ class BlueskyTest:
     async def smoke_test(self) -> str:
         password = dag.set_secret("test", "test")
         return await dag.bluesky().post("test@test.com", password, "Hello, world!")
-                           
+           
     @function
     async def all(self) -> str:
         output = ""
         output += await self.smoke_test()
+
+        return output
+
+    @function
+    async def text_examples(self, source: dagger.Directory) -> str:
+        output = ""
+        for language in ["python", "typescript", "go", "shell"]:
+            output += await (
+                dag.
+                dagger_dev().
+                dev(language).
+                with_mounted_directory("/src", source).
+                with_workdir("/src").
+                with_exec(["bash", "-c", f"dagger call -m examples/{language} bluesky-post"]).
+                stdout()
+            )
 
         return output
