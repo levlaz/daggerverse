4d146a7a5cf5027ca653ebb79bac49a434bda4a4
Lev <lev@levlaz.org>
2025-01-16 20:53:13 -0800

    [bluesky] bump to 0.15.2

--- bluesky/src/index.ts
+++ bluesky/src/index.ts
@@ -1,40 +1,50 @@
 /**
- * Bluesky Dagger Module 
- * 
+ * Bluesky Dagger Module
+ *
  * Send posts to Bluesky from your Dagger pipelines
  */
-import { dag, Container, Directory, object, func, Secret } from "@dagger.io/dagger"
-import {AtpAgent, RichText} from "@atproto/api"
+import {
+  dag,
+  Container,
+  Directory,
+  object,
+  func,
+  Secret,
+} from "@dagger.io/dagger";
+import { AtpAgent, RichText } from "@atproto/api";
 
 @object()
 /**
  * Bluesky Dagger Module
  */
-class Bluesky {
+export class Bluesky {
   /**
    * Send post to Bluesky
    */
   @func()
   async post(
-    email: string, 
-    password: Secret, 
-    text: string, 
-    host = "https://bsky.social"
+    email: string,
+    password: Secret,
+    text: string,
+    host = "https://bsky.social",
   ): Promise<string> {
-    const agent = new AtpAgent({service: host})
+    const agent = new AtpAgent({ service: host });
 
-    await agent.login({identifier: email, password: await password.plaintext()})
+    await agent.login({
+      identifier: email,
+      password: await password.plaintext(),
+    });
 
-    const rt = new RichText({text})
-    await rt.detectFacets(agent)
+    const rt = new RichText({ text });
+    await rt.detectFacets(agent);
     const postRecord = {
       $type: "app.bsky.feed.post",
       text: rt.text,
       facets: rt.facets,
-      createdAt: new Date().toISOString(),  
-    }
+      createdAt: new Date().toISOString(),
+    };
 
-    const {uri} = await agent.post(postRecord)
-    return uri
+    const { uri } = await agent.post(postRecord);
+    return uri;
   }
 }
