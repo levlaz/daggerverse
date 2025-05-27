47e8418227838f0267f116f41aa28f9fed0587de
Lev Lazinskiy <lev@levlaz.org>
2025-05-27 15:54:52 -0700

    release [bluesky/v0.4.2]
    * use proper inline doc format

--- bluesky/src/index.ts
+++ bluesky/src/index.ts
@@ -14,24 +14,26 @@ import {
 import { AtpAgent, RichText } from "@atproto/api";
 import { chunkAndPost, chunkText } from '@/lib/bluesky-chunker';
 
-@object()
 /**
  * Bluesky Dagger Module
  */
+@object()
 export class Bluesky {
   /**
    * Send post to Bluesky, if longer than 300 characters, it will be chunked into multiple posts.
+   * 
+   * @param email - Email address of the Bluesky account
+   * @param password - Password of the Bluesky account, should be a Dagger Secret
+   * @param text - Content to post on Bluesky
+   * @param host - Host of the Bluesky instance, defaults to the public instance
+   * 
    * @returns JSON string of posts
    */
   @func()
   async post(
-    // Email address of the Bluesky account
     email: string,
-    // Password of the Bluesky account, should be a Dagger Secret
     password: Secret,
-    // Content to post on Bluesky
     text: string,
-    // Host of the Bluesky instance, defaults to the public instance
     host = "https://bsky.social",
   ): Promise<string> {
     const agent = new AtpAgent({ service: host });
@@ -43,8 +45,6 @@ export class Bluesky {
 
     const rt = new RichText({ text });
     await rt.detectFacets();
-    const richChunks = chunkText(rt);
-
 
     const posts  = await chunkAndPost(agent, rt)
 
